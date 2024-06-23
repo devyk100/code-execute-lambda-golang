601fdd9c171e72f91297d331556b54e67b0cb3da
yash <devyk1000@gmail.com>
2024-06-23 09:24:33 +0530

    trying to add support for inputs

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"time"
@@ -15,14 +16,23 @@ import (
 type RequestBody struct {
 	Language string `json:"language"`
 	Code     string `json:"code"`
+	Inputs   string `json:"input"`
 }
 
-func pyExec(Code string) string {
+func pyExec(Code string, Inputs string) string {
 	err := os.WriteFile("/tmp/main.py", []byte(Code), 0777)
 	if err != nil {
 		return err.Error()
 	}
 	cmd := exec.Command("python3", "/tmp/main.py")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err.Error()
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, Inputs)
+	}()
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return string(output) + "\n" + err.Error()
@@ -31,12 +41,20 @@ func pyExec(Code string) string {
 	return string(output)
 }
 
-func nodeExec(Code string) string {
+func nodeExec(Code string, Inputs string) string {
 	err := os.WriteFile("/tmp/index.js", []byte(Code), 0777)
 	if err != nil {
 		return err.Error()
 	}
 	cmd := exec.Command("node", "/tmp/index.js")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err.Error()
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, Inputs)
+	}()
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return string(output) + "\n" + err.Error()
@@ -44,12 +62,20 @@ func nodeExec(Code string) string {
 	return string(output)
 }
 
-func cppExec(Code string) string {
+func cppExec(Code string, Inputs string) string {
 	err := os.WriteFile("/tmp/main.cpp", []byte(Code), 0777)
 	if err != nil {
 		return err.Error()
 	}
 	cmd := exec.Command("g++", "/tmp/main.cpp", "-o", "/tmp/a.out")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err.Error()
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, Inputs)
+	}()
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return string(output) + "\n" + err.Error()
@@ -62,12 +88,20 @@ func cppExec(Code string) string {
 	return string(output2)
 }
 
-func javaExec(Code string) string {
+func javaExec(Code string, Inputs string) string {
 	err := os.WriteFile("/tmp/main.java", []byte(Code), 0755)
 	if err != nil {
 		return err.Error()
 	}
 	cmd := exec.Command("java", "/tmp/main.java")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err.Error()
+	}
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, Inputs)
+	}()
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return string(output) + "\n" + err.Error()
@@ -75,15 +109,15 @@ func javaExec(Code string) string {
 	return string(output)
 }
 
-func codeExecutor(Language string, Code string, output chan string) {
+func codeExecutor(Language string, Code string, output chan string, Inputs string) {
 	if Language == "python" {
-		output <- pyExec(Code)
+		output <- pyExec(Code, Inputs)
 	} else if Language == "java" {
-		output <- javaExec(Code)
+		output <- javaExec(Code, Inputs)
 	} else if Language == "c++" {
-		output <- cppExec(Code)
+		output <- cppExec(Code, Inputs)
 	} else if Language == "node.js" {
-		output <- nodeExec(Code)
+		output <- nodeExec(Code, Inputs)
 	} else {
 		output <- "Invalid input"
 	}
@@ -108,7 +142,7 @@ func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.A
 	}
 
 	outputChan := make(chan string)
-	go codeExecutor(requestBody.Language, requestBody.Code, outputChan)
+	go codeExecutor(requestBody.Language, requestBody.Code, outputChan, requestBody.Inputs)
 	go timeOut(12, outputChan)
 	output := <-outputChan
 	var trimmedOutput string = ""
